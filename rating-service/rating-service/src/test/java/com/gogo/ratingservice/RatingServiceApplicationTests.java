4f9257dbd4a60e782ef14973be946cf5c004d540
Greattony Oduche <greattony96@gmail.com>
2023-04-19 15:45:27 +0300

    initial commit

---
+++ rating-service/rating-service/src/test/java/com/gogo/ratingservice/RatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gogo.ratingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
