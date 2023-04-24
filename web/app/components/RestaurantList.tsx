b7886d5a56da54d54b0554831c0c7960a99f5b6f
Greattony Oduche <greattony96@gmail.com>
2023-04-24 07:43:41 +0300

    style: individual restaurant and store page

--- web/app/components/RestaurantList.tsx
+++ web/app/components/RestaurantList.tsx
@@ -12,7 +12,7 @@ export default function RestaurantList({ restaurants }) {
                             <img className="h-40 w-full object-cover md:w-48" src={`${restaurant?.image}`} alt="Food Image" />
                         </div>
                         <div className="p-8">
-                            <div className="uppercase tracking-wide text-sm text-amber-400 font-semibold">&#9733; 4.88</div>
+                            <div className="uppercase tracking-wide text-sm text-amber-600 font-semibold">&#9733; 4.88</div>
                             <p className="block mt-1 text-lg leading-tight font-medium text-black">{restaurant.name}</p>
                             <p className="mt-2 text-gray-500">Breakfast - &#8378;&#8378;</p>
                         </div>
