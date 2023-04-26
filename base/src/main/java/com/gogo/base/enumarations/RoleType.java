16af1dc2d546fcb43ecec56bc8b6aed42e21f214
Greattony Oduche <greattony96@gmail.com>
2023-04-27 02:11:05 +0300

    build: account service migration

---
+++ base/src/main/java/com/gogo/base/enumarations/RoleType.java
@@ -0,0 +1,5 @@
+package com.gogo.base.enumarations;
+
+public enum RoleType {
+    ROLE_ADMIN, ROLE_USER, ROLE_PARTNER, ROLE_COURIER
+}
---
+++ base/src/main/java/com/gogo/base/models/Role.java
@@ -0,0 +1,22 @@
+package com.gogo.base.models;
+
+import com.gogo.base.enumarations.RoleType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private RoleType name;
+}
