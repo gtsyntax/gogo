c5e15b3fe3343f78cfef6a1ef0f18a87f13c0411
Greattony Oduche <greattony96@gmail.com>
2023-04-27 05:20:52 +0300

    feat: review entity and repository

--- base/src/main/java/com/gogo/base/repository/CartRepository.java
+++ base/src/main/java/com/gogo/base/repository/CartRepository.java
@@ -3,10 +3,12 @@ package com.gogo.base.repository;
 import com.gogo.base.enumerations.CartStatus;
 import com.gogo.base.models.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface CartRepository extends JpaRepository<Cart, UUID> {
     List<Cart> findByStatus(CartStatus status);
     List<Cart> findByStatusAndUserId(CartStatus status, UUID userId);
--- base/src/main/java/com/gogo/base/repository/ProductRepository.java
+++ base/src/main/java/com/gogo/base/repository/ProductRepository.java
@@ -4,10 +4,12 @@ import com.gogo.base.models.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
     Page<Product> findAll(Pageable pageable);
     List<Product> findByShopId(UUID id);
