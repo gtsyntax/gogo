a293de8fad0b8d8d40b692d13092cb6ac64967f0
IremG <isn.gencoglu@gmail.com>
2023-05-09 22:25:20 +0300

    shop Service

--- base/src/main/java/com/gogo/base/controllers/ShopController.java
+++ base/src/main/java/com/gogo/base/controllers/ShopController.java
@@ -3,13 +3,19 @@ package com.gogo.base.controllers;
 import com.gogo.base.dto.ShopDetail;
 import com.gogo.base.dto.ShopResponse;
 import com.gogo.base.dto.ShopUpdate;
+import com.gogo.base.models.Shop;
+import com.gogo.base.repository.ShopRepository;
 import com.gogo.base.services.ShopService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -17,6 +23,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class ShopController {
     private final ShopService shopService;
+    private final ShopRepository shopRepository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -25,9 +32,24 @@ public class ShopController {
     }
 
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
-    public List<ShopResponse> getAllStores() {
-        return shopService.getAllStores();
+    public ResponseEntity<Map<String, Object>> getAllProducts(@RequestParam(defaultValue = "0") int page,
+                                                              @RequestParam(defaultValue = "15") int size) {
+
+        try {
+            Pageable paging = PageRequest.of(page, size);
+
+            Page<Shop> pageTuts;
+            pageTuts = shopRepository.findAll(paging);
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("products", pageTuts.getContent());
+            response.put("currentPage", pageTuts.getNumber());
+            response.put("totalItems", pageTuts.getTotalElements());
+            response.put("totalPages", pageTuts.getTotalPages());
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/{id}")
@@ -48,8 +70,4 @@ public class ShopController {
         shopService.deleteShop(id);
     }
 
-    @PostMapping("/items/{id}/add")
-    public void addProductToCart(@PathVariable("id") UUID id) {
-        shopService.addProductToCart(id);
-    }
 }
