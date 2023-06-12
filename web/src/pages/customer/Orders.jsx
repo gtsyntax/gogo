ac556e8750b3498a2e262857b597e2dab5fd391e
Greattony Oduche <greattony96@gmail.com>
2023-06-12 22:28:24 +0300

    demo and old web removed

--- web/src/pages/customer/Orders.jsx
+++
@@ -1,8 +0,0 @@
-export default function CustomerOrders(){
-    return (
-        <main className="p-8">
-            <h1 className="mb-4 text-2xl font-semibold">Previous Orders</h1>
-            <hr/>
-        </main>
-    )
-}
--- web/src/pages/customer/Reviews.jsx
+++
@@ -1,8 +0,0 @@
-export default function CustomerReviews(){
-    return (
-        <main className="p-8">
-            <h1 className="mb-4 text-2xl font-semibold">Reviews</h1>
-            <hr/>
-        </main>
-    )
-}
